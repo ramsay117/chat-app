502a6017ccb1f2430672c6ec5ad4c6eb227016a8
BE-Shubham <shubham.gupta@smartjoules.in>
2024-03-25 01:46:25 +0530

    Place wildcard route at the bottom

--- backend/server.js
+++ backend/server.js
@@ -23,13 +23,13 @@ app.use(
 );
 app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-});
-
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoute);
 app.use('/api/users', userRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, async () => {
