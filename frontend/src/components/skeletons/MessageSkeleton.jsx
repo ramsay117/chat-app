8b7ce52ae20a23a05a1ae92278abe08d6ad8dfb7
Shubham Gupta <11shubhamgupta99@gmail.com>
2025-02-24 22:24:27 +0530

    refactor: Improve message skeleton UI with chat-based layout

--- frontend/src/components/skeletons/MessageSkeleton.jsx
+++ frontend/src/components/skeletons/MessageSkeleton.jsx
@@ -1,20 +1,24 @@
 const MessageSkeleton = () => {
   return (
-    <>
-      <div className='flex gap-3 items-center'>
-        <div className='skeleton w-10 h-10 rounded-full shrink-0'></div>
-        <div className='flex flex-col gap-1'>
-          <div className='skeleton h-4 w-40'></div>
-          <div className='skeleton h-4 w-40'></div>
+    <div className="space-y-4">
+      {/* Received message skeleton */}
+      <div className="chat chat-start">
+        <div className="chat-image avatar">
+          <div className="skeleton w-10 h-10 rounded-full" />
         </div>
+        <div className="chat-bubble skeleton w-52 h-10" />
+        <div className="chat-footer skeleton w-12 h-4 mt-1" />
       </div>
-      <div className='flex gap-3 items-center justify-end'>
-        <div className='flex flex-col gap-1'>
-          <div className='skeleton h-4 w-40'></div>
+
+      {/* Sent message skeleton */}
+      <div className="chat chat-end">
+        <div className="chat-image avatar">
+          <div className="skeleton w-10 h-10 rounded-full" />
         </div>
-        <div className='skeleton w-10 h-10 rounded-full shrink-0'></div>
+        <div className="chat-bubble skeleton w-40 h-8" />
+        <div className="chat-footer skeleton w-12 h-4 mt-1" />
       </div>
-    </>
+    </div>
   );
 };
 
