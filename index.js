48c1caaa20432ee058ec3a1dc04b917a6834a319
Shubham Gupta <11shubhamgupta99@gmail.com>
2025-03-02 16:35:37 +0530

    chore: Remove root index.js and update background image

--- index.js
+++
@@ -1,5 +0,0 @@
-// Root index.js - Redirects to backend server
-import './backend/server.js';
-
-// This file helps Vercel identify the main entry point when using "use" configuration
-// in vercel.json with @vercel/node
